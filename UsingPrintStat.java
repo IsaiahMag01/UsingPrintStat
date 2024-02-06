61fb092578d53e6a063f2cebe35f6ae8c56cbcd4
Isaiah Magana <isaiahm@isaiahs-laptop.myfiosgateway.com>
2024-02-06 00:13:10 -0500

    Create initial project

---
+++ UsingPrintStat.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ */
+
+package com.maganadev.usingprintstat;
+
+/**
+ *
+ * @author isaiahm
+ */
+public class UsingPrintStat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
