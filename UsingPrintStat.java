003eee9e11bb894b467e0379e9d02996093c3a17
Isaiah Magana <isaiahm@isaiahs-laptop.myfiosgateway.com>
2024-02-06 00:15:03 -0500

    Write Java doc comments for the program

--- UsingPrintStat.java
+++ UsingPrintStat.java
@@ -1,15 +1,15 @@
-/*
- * 
- */
-
 package com.maganadev.usingprintstat;
-
 /**
- *
+ * Show the diffrence between print and println just as done with codecademy lecture
  * @author isaiahm
+ * 
+ * UsingPrintStat.java
  */
 public class UsingPrintStat {
-
+    /**
+     * Main method to run program
+     * @param args 
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
